036bf9fd61bb4db7ccabca9e01e4481a53940c7c
ksebil2301 <ksebiluyanik@gmail.com>
2021-01-31 04:05:57 +0300

    Add project files.

---
+++ MyDictonary/MyDictonary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictonary<TKey, TValue>
+    {
+        TKey[] Tkeys;
+        TValue[] Tvalues;
+        public MyDictonary()
+        {
+            Tkeys = new TKey[0];
+            Tvalues = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = Tkeys;
+            TValue[] tempValue = Tvalues;
+            Tkeys = new TKey[tempKey.Length + 1];
+            Tvalues = new TValue[tempValue.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Tkeys[i] = tempKey[i];
+                Tvalues[i] = tempValue[i];
+            }
+            Tkeys[Tkeys.Length - 1] = key;
+            Tvalues[Tvalues.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < Tkeys.Length; i++)
+            {
+                Console.WriteLine(Tkeys[i] + " " + Tvalues[i]);
+            }
+        }
+    }
+
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string, int> Emergency = new MyDictonary<string, int>();
+            Emergency.Add("Ambulance", 112);
+            Emergency.Add("Fire", 110);
+            Emergency.Add("Police", 155);
+            Emergency.Add("Gendarme", 156);
+            Emergency.List();
+        }
+    }
+}
